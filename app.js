c3403341f3dbf6f4ac87f5fa5ce4f8aac57de3c0
zhaojing834 <zhaojing834@jd.com>
2023-02-21 09:49:13 +0800

    express模版完善

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express')
+const router = require('./router')
 
 // 加一个注释，用以说明，本项目代码可以任意定制更改
 const app = express()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3009
 
 // 挂载路由
 app.use('/api', router)
+// 数据转json的中间件挂载
+app.use(express.json())
 
 // 挂载统一处理服务端错误中间件
-app.use(errorHandler())
+// app.use(errorHandler())
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`)
--- router/index.js
+++ router/index.js
@@ -1,3 +1,10 @@
 const express = require('express')
 
 const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    console.log(req.method)
+    res.send('/index')
+})
+
+module.exports = router
